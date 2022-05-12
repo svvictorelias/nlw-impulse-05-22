06a5f50b1629ef2f18c6bd903ff35f0e60362372
Victor Elias <svvictorelias@gmail.com>
2022-05-12 17:23:07 -0300

    adicionado opções de feedback e separado componentes

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+import bugImage from '../../assets/bug.svg'
+import thoughtImage from '../../assets/thought.svg'
+import ideaImage from '../../assets/idea.svg'
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
+import { FeedbackContentStep } from './Steps/FeedbackContentStep'
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImage,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImage,
+      alt: 'Imagem de uma lampada representando ideia'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImage,
+      alt: 'Imagem de uma nuvem representando um balão de pensamento'
+    }
+  }
+}
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+  return (
+    <>
+      <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+        {!feedbackType ? (
+          <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+        ) : (
+          <FeedbackContentStep feedbackType={feedbackType} />
+        )}
+
+        <footer className="text-xs text-neutral-400">
+          Feito pelo{' '}
+          <a
+            className="underline underline-offset-2"
+            href="https://www.linkedin.com/in/svvictorelias/"
+            target="_blank"
+          >
+            /svvictorelias
+          </a>
+        </footer>
+      </div>
+    </>
+  )
+}
